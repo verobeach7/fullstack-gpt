aeb91475f64b649a01ecf0cc0e0ac563e2377230
verobeach7 <60215757+verobeach7@users.noreply.github.com>
2025-01-05 22:44:55 +0900

    Fullstack-GPT: LangChain - DOCUMENT GPT(Data Flow)

--- Home.py
+++ Home.py
@@ -1,41 +1,25 @@
 import streamlit as st
-from langchain.prompts import PromptTemplate
+from datetime import datetime
 
-# st.write는 AI로부터 받은 것이 무엇이든지 볼 수 있음
+today = datetime.today().strftime("%H:%M:%S")
 
-# write string
-st.write("Hello")
+st.title(today)
 
-# write array
-st.write([1, 2, 3, 4])
+model = st.selectbox("Choose your model", ("GPT-3", "GPT-4"))
 
-# write dictionary
-st.write({"x": 1})
+st.write("You selected: ", model)
 
-# write class: Library에 있는 클래스에 대한 것을 볼 수 있음
-# 클래스 내에 설명 문서가 작성되어 있는 경우 이도 볼 수 있음
-# 클래스 내에 property를 확인할 수 있음
-st.write(PromptTemplate)
+if model == "GPT-3":
+    st.write("cheap")
+else:
+    st.write("not cheap")
+    name = st.text_input("What is your name?")
+    st.write(name)
 
-p = PromptTemplate.from_template("xxxx")
+    value = st.slider(
+        "temperature",
+        min_value=0.1,
+        max_value=1.0,
+    )
 
-st.write(p)
-
-# Magic: 변수명만 적으면 자동으로 출력해줌
-# 장점은 간단하게 변수명만 작성하면 되지만 단점은 이것이 화면에 출력한다는 의미를 코드에 담지 못함
-
-a = [5, 6, 7]
-
-d = {"y": 10}
-
-a
-
-d
-
-PromptTemplate
-
-p
-
-option = st.selectbox("Choose your model", ("GPT-3", "GPT-4"))
-
-st.write("You selected: ", option)
+    st.write(value)
