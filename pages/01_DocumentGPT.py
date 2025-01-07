936ab4ec19b21513a706d001a73513e5aff4f695
verobeach7 <60215757+verobeach7@users.noreply.github.com>
2025-01-07 21:59:16 +0900

    Fullstack-GPT: LangChain - DOCUMENT GPT(Chat Messages: Memorize chat by session_state)

--- pages/01_DocumentGPT.py
+++ pages/01_DocumentGPT.py
@@ -8,19 +8,34 @@ st.set_page_config(
 
 st.title("DocumentGPT")
 
-with st.chat_message("human"):
-    st.write("Helloooo")
+if "messages" not in st.session_state:
+    st.session_state["messages"] = []
 
-with st.chat_message("ai"):
-    st.write("How are you?")
+# st.write(st.session_state["messages"])
 
-st.chat_input("Send a message to the ai")
 
-with st.status("Embedding file...", expanded=True) as status:
-    time.sleep(2)
-    st.write("Getting the file")
-    time.sleep(2)
-    st.write("Embedding the file")
-    time.sleep(2)
-    st.write("Caching the file")
-    status.update(label="Error", state="error")
+def send_message(message, role, save=True):
+    with st.chat_message(role):
+        st.write(message)
+    if save:
+        st.session_state["messages"].append(
+            {
+                "message": message,
+                "role": role,
+            }
+        )
+
+
+for message in st.session_state["messages"]:
+    send_message(
+        message["message"],
+        message["role"],
+        save=False,
+    )
+
+message = st.chat_input("Send a message to the ai")
+
+if message:
+    send_message(message, role="human")
+    time.sleep(1)
+    send_message(f"You said: {message}", "ai")
