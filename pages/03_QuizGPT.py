0ee4ab640d2bf30047cb7212875eafedcddce149
verobeach7 <60215757+verobeach7@users.noreply.github.com>
2025-01-17 18:25:01 +0900

    Fullstack-GPT: QuizGPT(Questions Prompt)

--- pages/03_QuizGPT.py
+++ pages/03_QuizGPT.py
@@ -3,6 +3,8 @@ from langchain.text_splitter import CharacterTextSplitter
 import streamlit as st
 from langchain.retrievers import WikipediaRetriever
 from langchain.chat_models import ChatOpenAI
+from langchain.prompts import ChatPromptTemplate
+from langchain.callbacks import StreamingStdOutCallbackHandler
 
 
 st.set_page_config(
@@ -15,7 +17,14 @@ st.title("QuizGPT")
 llm = ChatOpenAI(
     temperature=0.1,
     model="gpt-4o-mini",
+    streaming=True,
+    callbacks=[StreamingStdOutCallbackHandler()],
 )
+
+
+# docs를 하나의 string으로 변경
+def format_docs(docs):
+    return "\n\n".join(document.page_content for document in docs)
 
 
 # file을 작은 chunk로 쪼갠 후 docs 반환
@@ -35,6 +44,7 @@ def split_file(file):
     return docs
 
 
+# 사이드바
 with st.sidebar:
     docs = None
     # selectbox를 이용해 file/wikipedia article 중 선택
@@ -53,15 +63,19 @@ with st.sidebar:
         )
         if file:
             docs = split_file(file)
-            # docs를 출력
-            st.write(docs)
     # Wikipedia Article 선택
     else:
         # 사용자로부터 topic 받기
         topic = st.text_input("Search Wikipedia...")
         if topic:
             # WikipediaRetriver 설정
+            # top_k_results: 몇 개의 문서를 가져올지 설정
             retriever = WikipediaRetriever(top_k_results=5)
+            # 한국어 문서를 얻기를 원할 때 lang을 설정할 수 있음
+            # retriever = WikipediaRetriever(
+            #     top_k_results=5,
+            #     lang="ko",
+            # )
             with st.status("Searching Wikipedia..."):
                 # topic 관련 문서 찾기
                 docs = retriever.get_relevant_documents(topic)
@@ -78,4 +92,43 @@ if not docs:
     """
     )
 else:
-    st.write(docs)
+    # 프롬프트에 퀴즈 예시를 제공
+    prompt = ChatPromptTemplate.from_messages(
+        [
+            (
+                "system",
+                """
+    You are a helpful assistant that is role playing as a teacher.
+         
+    Based ONLY on the following context make 10 questions to test the user's knowledge about the text.
+    
+    Each question should have 4 answers, three of them must be incorrect and one should be correct.
+         
+    Use (o) to signal the correct answer.
+         
+    Question examples:
+         
+    Question: What is the color of the ocean?
+    Answers: Red|Yellow|Green|Blue(o)
+         
+    Question: What is the capital or Georgia?
+    Answers: Baku|Tbilisi(o)|Manila|Beirut
+         
+    Question: When was Avatar released?
+    Answers: 2007|2001|2009(o)|1998
+         
+    Question: Who was Julius Caesar?
+    Answers: A Roman Emperor(o)|Painter|Actor|Model
+         
+    Your turn!
+         
+    Context: {context}
+""",
+            )
+        ]
+    )
+    # invoke시 들어온 docs를 format_docs function을 이용해 string으로 변경
+    chain = {"context": format_docs} | prompt | llm
+    start = st.button("Generate Quiz")
+    if start:
+        chain.invoke(docs)
