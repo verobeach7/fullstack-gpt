0bb161bd08e7a2414065dcd4d02872f0a4237e24
verobeach7 <60215757+verobeach7@users.noreply.github.com>
2025-01-17 16:16:35 +0900

    Fullstack-GPT: QuizGPT(WikipediaRetriever)

--- pages/03_QuizGPT.py
+++ pages/03_QuizGPT.py
@@ -1,3 +1,60 @@
+from langchain.document_loaders import UnstructuredFileLoader
+from langchain.text_splitter import CharacterTextSplitter
 import streamlit as st
+from langchain.retrievers import WikipediaRetriever
+
+
+st.set_page_config(
+    page_title="QuizGPT",
+    page_icon="❓",
+)
 
 st.title("QuizGPT")
+
+
+# file을 작은 chunk로 쪼갠 후 docs 반환
+@st.cache_data(show_spinner="Loading file...")
+def split_file(file):
+    file_content = file.read()
+    file_path = f"./.cache/quiz_files/{file.name}"
+    with open(file_path, "wb") as f:
+        f.write(file_content)
+    splitter = CharacterTextSplitter.from_tiktoken_encoder(
+        separator="\n",
+        chunk_size=600,
+        chunk_overlap=100,
+    )
+    loader = UnstructuredFileLoader(file_path)
+    docs = loader.load_and_split(text_splitter=splitter)
+    return docs
+
+
+with st.sidebar:
+    # selectbox를 이용해 file/wikipedia article 중 선택
+    choice = st.selectbox(
+        "Choose what you want to use.",
+        (
+            "File",
+            "Wikipedia Article",
+        ),
+    )
+    # File 선택
+    if choice == "File":
+        file = st.file_uploader(
+            "Upload a .docx , .txt or .pdf file",
+            type=["pdf", "txt", "docx"],
+        )
+        if file:
+            docs = split_file(file)
+            # docs를 출력
+            st.write(docs)
+    # Wikipedia Article 선택
+    else:
+        # 사용자로부터 topic 받기
+        topic = st.text_input("Search Wikipedia...")
+        if topic:
+            # WikipediaRetriver 설정
+            retriever = WikipediaRetriever(top_k_results=5)
+            with st.status("Searching Wikipedia..."):
+                # topic 관련 문서 찾기
+                docs = retriever.get_relevant_documents(topic)
