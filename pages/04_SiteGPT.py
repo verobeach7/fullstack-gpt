a5ce7b414eb4e02ddd7fbdb19fa7109c617f3eed
verobeach7 <60215757+verobeach7@users.noreply.github.com>
2025-01-18 23:47:26 +0900

    Fullstack-GPT: SiteGPT(SitemapLoader: by using sitemap.xml)

--- pages/04_SiteGPT.py
+++ pages/04_SiteGPT.py
@@ -1,13 +1,23 @@
-from langchain.document_loaders import AsyncChromiumLoader
-from langchain.document_transformers import Html2TextTransformer
+from langchain.document_loaders import SitemapLoader
 import streamlit as st
+
+# import requests
 
 st.set_page_config(
     page_title="SiteGPT",
     page_icon="🖥️",
 )
 
-html2text_transformer = Html2TextTransformer()
+
+@st.cache_data(show_spinner="Loading website...")
+def load_website(url):
+    # SitemapLoader는 sitemap.xml에 들어있는 모든 페이지로부터 문서를 가져옴
+    loader = SitemapLoader(url)
+    # 초당 요청 수를 제한하여 차단 정책이나 속도 제한 정책 위반을 방지
+    loader.requests_per_second = 5
+    docs = loader.load()
+    return docs
+
 
 st.markdown(
     """
@@ -28,7 +38,15 @@ with st.sidebar:
 
 
 if url:
-    loader = AsyncChromiumLoader([url])
-    docs = loader.load()
-    transformed = html2text_transformer.transform_documents(docs)
-    st.write(transformed)
+    # url에 .xml이 포함되어 있지 않은 경우 url을 재입력하게 함
+    if ".xml" not in url:
+        with st.sidebar:
+            st.error("Please write down a Sitemap URL.")
+    else:
+        # 요청 응답이 xml인지 html인지 확인하는데 사용
+        # response = requests.get(url)
+        # print(response.text)
+
+        # url로부터 모든 페이지를 가져옴
+        docs = load_website(url)
+        st.write(docs)
